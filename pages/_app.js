812c2b6f1b08c458e365b1bd05e3e706474f36a6
Cris Fandino <sicrnujnuj@gmail.com>
2020-12-24 15:18:36 +0800

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import Layout from './_layout'
 import '../styles/globals.css'
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
+
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout title={`Covid Tracker ${new Date().getFullYear()}`}>
